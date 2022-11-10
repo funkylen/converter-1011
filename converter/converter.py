48f51e0f33c27dcd62a8c6e0a94a2a21a5cc6c5a
funkylen <funkylen@gmail.com>
2022-11-10 17:24:50 +0300

    finish converter

--- converter/converter.py
+++ converter/converter.py
@@ -1,70 +1,37 @@
-import prompt
+DATA = [
+    {
+        'type': 'cm',
+        'key': 1,
+        'pow': -2,
+    },
+    {
+        'type': 'km',
+        'key': 2,
+        'pow': 3,
+    },
+    {
+        'type': 'dm',
+        'key': 3,
+        'pow': -1,
+    },
+    {
+        'type': 'm',
+        'key': 4,
+        'pow': 0,
+    },
+    {
+        'type': 'mm',
+        'key': 5,
+        'pow': -3,
+    }
+]
 
 
-
-def run():
-    data = [
-        {
-            'type': 'cm',
-            'key': 1,
-            'pow' : -2,
-        },
-        {
-            'type': 'km',
-            'key': 2,
-            'pow' : 3,
-        },
-        {
-            'type': 'dm',
-            'key': 3,
-            'pow' : -1,
-        },
-        {
-            'type': 'm',
-            'key': 4,
-            'pow' : 0,
-        },
-        {
-            'type': 'mm',
-            'key': 5,
-            'pow' : -3,
-        }
-    ]
-
-
-
-    message = "Выберите:\n"
-    for item in data:
-        type = item.get('type')
-        key = item.get('key')
-        
-        message += f"{key}. {type}\n"
-    
-
-    option1 = prompt.integer(message)
-
-    message = "Выберите :\n"
-    for item in data:
-        type = item.get('type')
-        key = item.get('key')
-        
-        message += f"{key}. {type}\n"
-    
-
-    option2 = prompt.integer(message)
-
-    number = prompt.real("Введите число: ")
-
-    for item in data:
+def run(option1, option2, number):
+    for item in DATA:
         if item.get('key') == option1:
             pow1 = item.get('pow')
-    for item in data:
+    for item in DATA:
         if item.get('key') == option2:
             pow2 = item.get('pow')
-    result = convert(number,pow1,pow2)
-    print(result)
-   
-
-def convert(num,pow1,pow2):
-    print(pow1,pow2)
-    return num * 10**-(pow2-pow1)
+    return number * 10**-(pow2-pow1)
--- converter/scripts/convert-distance.py
+++ converter/scripts/convert-distance.py
@@ -1,8 +1,17 @@
+import argparse
 from converter.converter import run
 
 
 def main():
-    run()
+    parser = argparse.ArgumentParser(description='Перевод единиц измерения')
+
+    parser.add_argument('--option1', required=True, type=int)
+    parser.add_argument('--option2', required=True, type=int)
+    parser.add_argument('--number', required=True, type=float)
+
+    args = parser.parse_args()
+
+    print(run(args.option1, args.option2, args.number))
 
 
 if __name__ == '__main__':
